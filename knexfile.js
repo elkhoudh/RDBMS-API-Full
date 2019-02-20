391470f4e7969be8ad550ff1f9db828fcc456bef
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-20 17:54:11 -0500

    Final Touches

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,7 @@ pg.defaults.ssl = true;
 module.exports = {
   development: {
     client: "pg",
-    connection:
-      "postgres://ortrrlreghbyfi:dfa04fde8db0fd43bbc2548a10b920688ab49d90c1e5ee2c1c107a460b4b56a4@ec2-54-243-128-95.compute-1.amazonaws.com:5432/dac4b9jqsflgi5",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
