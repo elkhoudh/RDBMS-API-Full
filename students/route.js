7278e5a7ed8bbf59e74c4c2e1681a3b8d3cc6196
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-20 17:26:31 -0500

    Added All Cohort and Student Routes

--- students/route.js
+++ students/route.js
@@ -34,35 +34,67 @@ route.post("/", (req, res) => {
   }
 });
 
-// route.get("/:id", (req, res) => {
-//   const { id } = req.params;
+route.get("/:id", (req, res) => {
+  const { id } = req.params;
 
-//   db("cohorts")
-//     .where({ id })
-//     .first()
-//     .then(cohort => {
-//       if (cohort) {
-//         res.json(cohort);
-//       } else {
-//         res.status(404).json({ message: "Cohort Not Found" });
-//       }
-//     })
-//     .catch(() => {
-//       res.status(500).json({ message: "Server Error" });
-//     });
-// });
+  db("students")
+    .where({ id })
+    .first()
+    .then(student => {
+      if (student) {
+        res.json(student);
+      } else {
+        res.status(404).json({ message: "student Not Found" });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Server Error" });
+    });
+});
 
-// route.get("/:id/students", (req, res) => {
-//   const { id } = req.params;
+route.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, cohort_id } = req.body;
 
-//   db("students")
-//     .where("cohort_id", id)
-//     .then(students => {
-//       res.json(students);
-//     })
-//     .catch(() => {
-//       res.status(500).json({ message: "Server Error" });
-//     });
-// });
+  if (!name || !cohort_id) {
+    res.status(422).json({ message: "Name and Cohort ID Required" });
+  } else {
+    db("students")
+      .where({ id })
+      .update({ name, cohort_id })
+      .then(result => {
+        if (result) {
+          db("students").then(students => {
+            res.json(students);
+          });
+        } else {
+          res.status(400).json({ message: "Failed to update student" });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({ message: "Server Error" });
+      });
+  }
+});
+
+route.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("students")
+    .where({ id })
+    .del()
+    .then(result => {
+      if (result) {
+        db("students").then(students => {
+          res.json(students);
+        });
+      } else {
+        res.status(400).json({ message: "Failed to delete Student" });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Server Error" });
+    });
+});
 
 module.exports = route;
